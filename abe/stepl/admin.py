16b03be4cebcae35e7940446ae20bb90bced356d
Quanzheng Long <qlong@purdue.edu>
2015-10-03 16:21:53 -0400

    change admin and add searching

--- abe/stepl/admin.py
+++ abe/stepl/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
 from .models import IndexInput
+from .models import CountyData
 
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("state_name","name","station_name")
+    search_fields = ['state_name']
+
+admin.site.register(CountyData, MemberAdmin)
 admin.site.register(IndexInput)
