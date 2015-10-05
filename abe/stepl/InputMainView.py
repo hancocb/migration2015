e11b88c06a70c966a5027efbc4a830d893884513
Quanzheng Long <qlong@purdue.edu>
2015-10-05 09:44:22 -0400

    uburn reference unoff input

--- abe/stepl/InputMainView.py
+++ abe/stepl/InputMainView.py
@@ -2,7 +2,7 @@ from django.views.generic import View
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
-from models import IndexInput,SoilDataInput,SoilData
+from models import IndexInput,SoilDataInput,SoilData,UrbanReferenceRunoff,UrbanReferenceRunoffInput
 from django.contrib.auth import authenticate, login
 
 class InputMainView(View):
@@ -35,14 +35,14 @@ class InputMainView(View):
         	context['range' + str(i)] = range( 1, i + 1 )
 
         #prepare data    
-        prepareOptMainView()
+        self.prepareOptMainView(request)
             
         return render(request, 'inputMain.html', { 'ctx':context, 'req' : request })
 
     def get(self, request):
     	raise Http404("GET of this page does not exist, you need POST")
 
-    def prepareOptMainView():
+    def prepareOptMainView(self,request):
         context = request.session
         #1. soil data
         ele = SoilData.objects.get(SHG='A')
@@ -57,8 +57,7 @@ class InputMainView(View):
                     )
                 soilInput.save()
         #Detailed urban reference runoff curve number
-        urban_types = [
-            "Commercial",
+        '''
             "Industrial",
             "Institutional",
             "Transportation",
@@ -67,16 +66,20 @@ class InputMainView(View):
             "Urban-Cultivated",
             "Vacant-Developed",
             "Open Space",
+        '''
+        urban_types = [
+            "Commercial",
             ]
         for uburn in urban_types:
-            if not UrbanReferenceRunoffInput.objects.filter(session_id=context['IndexInput']['id'],watershd_id = i).exists():
-
+            if not UrbanReferenceRunoffInput.objects.filter(session_id=context['IndexInput']['id'],Urban=uburn).exists():
+                ele = UrbanReferenceRunoff.objects.get(Urban=uburn)
                 soilInput = UrbanReferenceRunoffInput(
-                        Soil_N_Conc =  ele.Soil_N_Conc,
-                        Soil_P_Conc =  ele.Soil_P_Conc,
-                        Soil_BOD_Conc =  ele.Soil_BOD_Conc,
+                        SHG_A = ele.SHG_A, 
+                        SHG_B = ele.SHG_B, 
+                        SHG_C = ele.SHG_C, 
+                        SHG_D = ele.SHG_D,
+                        Urban = ele.Urban,
                         session_id = context['IndexInput']['id'] ,
-                        watershd_id = i
                     )
                 soilInput.save()
 
