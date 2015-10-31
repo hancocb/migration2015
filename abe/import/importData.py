4d55d3aee5a7fcdac5fc8c514a1c76c6fbf7bcc7
Quanzheng Long <qlong@purdue.edu>
2015-10-31 17:34:10 -0400

    finish importDetailedReferenceRunoff

--- abe/import/importData.py
+++ abe/import/importData.py
@@ -6,7 +6,15 @@ from django.http import Http404
 from stepl.models import *
 from stepl.templatetags.simple_tags import twonum
 from stepl.tools import extract
-
+      
+    
+def importSoilTexture(data):
+    for i in range(1,11):
+        textureClass = Soil_Textural_Class_Choices[i-1][0]
+        dryDencity = data['GullyDB_'+twonum(i)+"1"]
+        correctFactor = data['GullyDB_'+twonum(i)+"2"]
+        s = SoilTexture(Standard=INPUT_STANDARD,Soil_Textural_Class=textureClass,Dry_Density=dryDencity,Correction_Factor=correctFactor)
+        s.save()
 def importReferenceRunoff(data):
     keyMap = (  ('Urban',   'urban'),
                 ('Cropland',    'crop'),
@@ -19,16 +27,27 @@ def importReferenceRunoff(data):
         r.SHG_B = data["CN_"+t[1]+"_B"]
         r.SHG_C = data["CN_"+t[1]+"_C"]
         r.SHG_D = data["CN_"+t[1]+"_D"]
-        r.save()       
-    
-def importSoilTexture(data):
-    for i in range(1,11):
-        textureClass = Soil_Textural_Class_Choices[i-1][0]
-        dryDencity = data['GullyDB_'+twonum(i)+"1"]
-        correctFactor = data['GullyDB_'+twonum(i)+"2"]
-        s = SoilTexture(Standard=INPUT_STANDARD,Soil_Textural_Class=textureClass,Dry_Density=dryDencity,Correction_Factor=correctFactor)
-        s.save()
-        
+        r.save() 
+def importDetailedReferenceRunoff(data):
+    keyMap = (
+            ('Commercial','comm'),
+            ('Industrial','indu'),
+            ('Institutional','inst'),
+            ('Transportation','tran'),
+            ('Multi-Family','mult'),
+            ('Single-Family','sing'),
+            ('Urban-Cultivated','urcu'),
+            ('Vacant-Developed','vade'),
+            ('Open Space',  'open')
+        )
+    for t in keyMap:
+        r = DetailedRunoff(Standard=INPUT_STANDARD,Urban=t[0])
+        r.SHG_A = data["CN_"+t[1]+"_A"]
+        r.SHG_B = data["CN_"+t[1]+"_B"]
+        r.SHG_C = data["CN_"+t[1]+"_C"]
+        r.SHG_D = data["CN_"+t[1]+"_D"]
+        r.save() 
+
 data = {}
 f = open("import/inputMainStaticData.txt","r")
 res = f.read()
@@ -44,6 +63,6 @@ for line in lines:
         exit()
 
 #importSoilTexture(data)
-importReferenceRunoff(data)
-
+#importReferenceRunoff(data)
+importDetailedReferenceRunoff(data)
 
