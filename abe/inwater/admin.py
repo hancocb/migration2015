5131aed41fc952e33d89e99ba5a59f1ddccae633
Q. Long <qlong@purdue.edu>
2016-01-20 15:15:40 -0500

    add search fields

--- abe/inwater/admin.py
+++ abe/inwater/admin.py
@@ -6,5 +6,6 @@ from .models import *
 class MonitoringAdmin(admin.ModelAdmin):
     list_display = ("gid", "name", "agency_type")
     ordering = ["name", "gid"]
+    search_fields = ['agency_type','agency_organization','name','description','user_email']
 
 admin.site.register(Monitoring, MonitoringAdmin)
